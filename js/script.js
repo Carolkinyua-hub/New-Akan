081d98729b6905522163b1b804673ce1a413b88f
Carolkinyua-hub <carolnkinyua@gmail.com>
2020-08-29 01:02:02 +0300

    Add row and collumns on the form

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function akanName(){
+    let bday=document.getElementById("dob");
+    console.log(bday) 
+    // let mm=parseInt(d.getMonth());
+    // let yy=parseInt(d.getFullYear());
+    // let cc=parseInt(yy.slice(0,1));
+    // let dd =parseInt(bday.getDate());
+    // let db=Math.round((((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
+   
+}
+
+
+
+
+document.write fuge
+
+
+
+
+
+// document.getElementById("check").innerHTML="Your Akan name is" +AkanName() +".";
