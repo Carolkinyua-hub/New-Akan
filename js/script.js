e1aa560b3a4941565e29f196688edc9c408c1f48
Carolkinyua-hub <carolnkinyua@gmail.com>
2020-09-01 08:26:53 +0300

    Add event function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,74 @@
-function akanName(){
-    let bday=document.getElementById("dob");
-    console.log(bday) 
-    // let mm=parseInt(d.getMonth());
-    // let yy=parseInt(d.getFullYear());
-    // let cc=parseInt(yy.slice(0,1));
-    // let dd =parseInt(bday.getDate());
-    // let db=Math.round((((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
-   
-}
+//back end logic//
 
 
 
 
-document.write fuge
+//front end logic//
+function submit(event) {
+    event.preventDefault();
+    console.log("Form is submitted");
+    let year = document.querySelector().value;
+    let month = document.querySelector().value;
+    let day = document.querySelector().value;
+    console.log({ year });
+    console.log({ month });
+    console.log({ day });
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function akanName(){
+//     let bday=document.getElementById("dob");
+//     console.log(bday) 
+//     // let mm=parseInt(d.getMonth());
+//     // let yy=parseInt(d.getFullYear());
+//     // let cc=parseInt(yy.slice(0,1));
+//     // let dd =parseInt(bday.getDate());
+//     // let db=Math.round((((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
+
+// }
+
+
+
+
 
 
 
