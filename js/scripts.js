c334400aee13f2f289641e97782016a43ff40dbe
Carolkinyua-hub <carolnkinyua@gmail.com>
2020-09-02 10:19:41 +0300

    add new jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,14 @@
-//back end logic//
 
+$(document).ready(function () {
+    $("h1").click(function () {
+        alert("This is a header.");
+    });
+});
 
 
 
-
-
-
-
-
-//front-end logic//
-
-
-// window.addEventListener("DOMcontentLoaded"), (event) => {
-//     console.log("DOM fully loaded and parsed");
-// }
-//create variable for form element=document.querySelector("elementid")
-function submit(event) {
-    let myForm = document.querySelector("#form");
-    myForm.addEventListener("submit");
-    event.preventDefault();
-    // var d = document.querySelector("#birthdate").value
-    // console.log("dob");
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function akanName(){
-//     let bday=document.getElementById("dob");
-//     console.log(bday) 
-//     // let mm=parseInt(d.getMonth());
-//     // let yy=parseInt(d.getFullYear());
-//     // let cc=parseInt(yy.slice(0,1));
-//     // let dd =parseInt(bday.getDate());
-//     // let db=Math.round((((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
-
-// }
-
-
-
-
-
-
-
-
-
-// document.getElementById("check").innerHTML="Your Akan name is" +AkanName() +".";
+$(document).ready(function () {
+    $("button").click(function (event) {
+        event.preventDefault();
+    });
+});
